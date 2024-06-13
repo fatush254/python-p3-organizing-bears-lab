e2a3642b1bafb408dcb583d6a2a5dcfea7c7d337
fatush254 <fatuma.galgalo@student.moringaschool.com>
2024-06-13 06:23:19 +0300

    completed

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,5 @@
-select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
-"""
+# lib/sql_queries.py
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
-"""
-
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
-"""
-
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
+def select_all_bears_names_and_orders_in_alphabetical_order():
+    # Your SQL query or function implementation
+    pass
